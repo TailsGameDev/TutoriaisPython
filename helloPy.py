e1eac799f81928d81473360f94f7a94707ec853c
Caina C. Caldas <cainacc@hotmail.com>
2019-02-20 15:20:55 -0300

    adding math file

--- helloPy.py
+++ helloPy.py
@@ -1,10 +1,2 @@
 # comando para escrever coisas na execucao
 print("Hello, Python!")
-
-#como fazer contas
-x = 2 + 4
-
-#x = x + 1
-x += 1
-
-print(x)
---
+++ math.py
@@ -0,0 +1,43 @@
+print("como fazer contas" + "\n")
+
+#concatenar nao eh matematica, mas eh muito importante. Significa colar
+#um texto no outro.
+print("Concatenacao")
+texto1 = "primeiro texto"
+texto2 = "segundo texto"
+x = texto1 + " " + texto2
+print(x)
+print("")
+
+print("Adicao")
+x = 2 + 4
+print(x)
+print("")
+
+print("Multiplicacao")
+x = 2 * 8
+print(x)
+print("")
+
+print("Divisao")
+x = 6 / 3
+print(x)
+print("")
+
+print("Usar variavel para defini-la")
+x = 1
+x = x + 1
+print(x)
+print("")
+
+#Avancado:
+
+print("Potenciacao")
+x = 5 ** 2
+print(x)
+print("")
+
+print("Radiciacao")
+x = 25 ** 0.5
+print(x)
+print("")
