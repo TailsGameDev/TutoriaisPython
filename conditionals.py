8d9aac963723e14f6ae40f040f142501be931732
Caina C. Caldas <cainacc@hotmail.com>
2019-02-20 14:20:39 -0300

    comentarios a mais

--- conditionals.py
+++ conditionals.py
@@ -1,10 +1,10 @@
-i = 3
+i = 11
 
-if i<4:
-    print("i eh menor que 4")
-elif i < 10:
-    print("i eh menor que 10")
-else:
+if i<4: # se i eh menor que 4
+    print("i eh menor que 4") #fazer isso
+elif i < 10: # se nao, mas i eh menor que 10
+    print("i eh menor que 10") #fazer isso
+else: #se nao
     print("i eh maior que 10")
 
 i = "peixe"
--- loops.py
+++ loops.py
@@ -2,11 +2,13 @@ i = 3
 
 #while is normal
 while i > 0:
-    i -= 1
-    print ("i vale " + str(i))
+    i = i - 1
+    print ("i vale " + str(i)) # str(i) transforma o i de inteiro para texto
 
 print("acabou")
 
+# for serve para percorrer um texto, executando algo para cada letra
+# ou faz o mesmo com vetores.
 #for is just an operator of vectors
 for x in "banana":
     print(x)
