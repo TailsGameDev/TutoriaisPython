41c5052f3075d33feb0b2d3c5e2707cb64794c8b
kkaldas <cainacc@hotmail.com>
2019-03-04 10:03:24 -0300

    Mais exemplos

--- vectors.py
+++ vectors.py
@@ -1,7 +1,19 @@
 # definindo vetor, ou lista
-vector = [1,2,3,4,7,9,8]
-
+vector = [1,2,4,6,8,9,15]
 print(vector)
 
 for x in vector:
+    print(x)
+
+soma = 0
+for x in vector:
+   soma = soma + x
+
+print("a soma dos elementos nesse vetor eh "+soma)
+
+vector2 = ["peixe", 3, 0.7, True]
+
+print(vector2)
+
+for x in vector2:
     print(x)
