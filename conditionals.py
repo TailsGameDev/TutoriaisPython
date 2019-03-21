af606eaf9d1292fe3dc4034a470aa1dfc5053d36
kkaldas <cainacc@hotmail.com>
2019-03-21 13:19:30 -0300

    convertendo bool para string

--- conditionals.py
+++ conditionals.py
@@ -19,7 +19,7 @@ variavelBooleana = 6*6 == 36
 print("6 * 6 eh 36? " + str(variavelBooleana))
 
 condicoesMultiplas = 6 < 7 and 7 < 8
-print("seis eh menor que sete e sete eh menor que oito? " + condicoesMultiplas)
+print("seis eh menor que sete e sete eh menor que oito? " + str(condicoesMultiplas))
 
 condicoesMultiplas = 6 < 7 or 1 >= 8
-print("6 eh menor que 7 ou 1 eh maior ou igual a 8? " + condicoesMultiplas)
+print("6 eh menor que 7 ou 1 eh maior ou igual a 8? " + str(condicoesMultiplas))
