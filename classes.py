27355d9aeeaeb40b4afda300a3aa84216430780d
kkaldas <cainacc@hotmail.com>
2019-04-25 17:01:01 -0300

    umas explicações a mais de classes

--- classes.py
+++ classes.py
@@ -1,7 +1,28 @@
+'''
+Para usar classes de outros arquivos, use from <nomeDoOutroArquivo> import <nomeDaClasse>
+Caso queira importar tudo que estiver no outro arquivo, use asterísco. Exemplo abaixo.
+Isso é muito importante! É a maneira para usar código que está escrito em outros arquivos!
+É crucial em projetos grandes.
+'''
 from moreClasses import *
 
+'''
+Classes são muito importantes e muito usadas na programação.
+Elas servem para organizar melhor os códigos, e torná-los mais fáceis
+de serem expandidos (ajudam o projeto a crescer ordenadamente)
+Classe é tipo um molde para criar vários objetos de acordo.
+Tipo você pode criar uma classe Guerreiro, que vai definir como
+os guerreiros são, e depois você pode criar vários guerreiros muito
+facilmente.
+Uma classe guarda atributos e métodos.
+Atributos são as características do objeto, coisas como cor, preço,
+peso, durabilidade, nome, etc.
+Métodos são coisas que algo é capaz de fazer, tipo atacar, mover,
+calcularProbabilidade, perderBraço, explodir.
+Não deixe de conferir o exemplo com o Guerreiro.py!! Está no repositório.
+'''
 class FirstClass:
-    #python nao permite construtores multiplos, mas permite default value
+    #abaixo está o método construtor, que define como os objetos são criados.
     def __init__ (self, arg1 = "batata", arg2 = "peixe"):
         self.atributo1 = arg1
         self.atributo2 = arg2
@@ -9,22 +30,27 @@ class FirstClass:
     def metodo(self):
         print("chamou metodo")
 
+# Aqui está acontecendo a criação de um objeto do tipo FirstClass!!
+# A variável que guarda esse objeto está sendo chamada de firstObj
 firstObj = FirstClass()
 
 print("atributo1: " + str(firstObj.atributo1))
 
+#chamando método do objeto.
 firstObj.metodo()
 
 secondObj = FirstClass("pudim", "chucrute")
 
 print("secondObj.atributo2: " + secondObj.atributo2)
 
+# Aqui estou criando um objeto de uma classe que está em outro arquivo!
+# Isso só é possível por causa do import láa no início desse código aqui.
 ObjFromAnotherFile = SecondClass()
 
 ObjFromAnotherFile.metodo()
 
 
-print("\n" + "Lets try inheritance!!")
+print("\n" + "Partiu testar a herança!!")
 
 class ChildClass(FirstClass):
     def __init__(self, arg1 = "batata", arg2 = "peixe", childArg = "childyAtribute"):
