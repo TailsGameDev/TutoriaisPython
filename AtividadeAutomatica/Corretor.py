e91ab6c0079e1dce4611168a6b71694ad9080b17
Caina <cainacc@hotmail.com>
2019-05-15 19:02:55 -0300

    corretor corrigido

--- AtividadeAutomatica/Corretor.py
+++ AtividadeAutomatica/Corretor.py
@@ -1,16 +1,7 @@
-#para funcionar tem que estar instalado no PC o pytest.
-#para verificar se estah instalado use comando no terminal: pip show pytest
-#se mostrar algo está instalado
-#se não estiver, usar comando pip install pytest
-#obs: o pip precisa estar instalado, mas instalei em todos os PCs menos o 4
+from Atividade import *
 
-#nao altere esse script!!!!!!!!!!
-
-import pytest
-
-from Gabarito import *
-
-def test_questao1_test():
+def test_questao1():
+    print("\nQuestao 1")
     #criando personagem e arma
     p = personagem()
     atkInicial = p.ataque
@@ -20,29 +11,47 @@ def test_questao1_test():
     #testando acrescimo de ataque
     p.equipar(a)
     atkEquipado = p.ataque
-    assert atkEquipado == atkInicial + bonus
+    ok= atkEquipado == atkInicial + bonus
+    if (ok):
+        print("[OK] item equipado com sucesso")
+    else:
+        print("[FAIL] problema ao equipar item")
     
     #testando se equipa duas armas
     p.equipar(a)
-    assert p.ataque == atkEquipado
-    print(p.ataque)
+    ok = p.ataque == atkEquipado
+    if (ok):
+        print("[OK] nao equipou duas armas")
+    else:
+        print("[FAIL] aparentemente estah equipando 2 armas")
 
     #testando se desequipou
     p.desequiparArma()
-    print(p.ataque)
-    assert p.ataque == atkInicial
-    assert atkInicial != atkEquipado
+    ok = p.ataque == atkInicial and atkInicial != atkEquipado
+    if (ok):
+        print("[OK] desequipou a arma com sucesso")
+    else:
+        print("[FAIL] problemas ao desequipar arma")
 
 def test_questao2():
+    print("\nQuestao 2")
     #um sobrevivente, 1kg por dia, 50 dias
     s = sobrevivente(1)
     list = [s]
-    assert s.quantosDiasSobreviveriamos(list) == 50
+    ok = s.quantosDiasSobreviveriamos(list) == 50
+    if (ok):
+        print("[OK] teste com um sobrevivente ok!")
+    else:
+        print("[FAIL] nao funciona para 1 sobrevivente na lista")
 
     #2 sobreviventes, 1kg por dia, 25 dias
     s2 = sobrevivente(1)
     list.append(s2)
-    assert s.quantosDiasSobreviveriamos(list) == 25
+    ok = s.quantosDiasSobreviveriamos(list) == 25
+    if (ok):
+        print("[OK] teste com 2 sobreviventes")
+    else:
+        print("[FAIL] 2 sobreviventes na lista")
 
     #4 sobreviventes, e o que faz a conta nao esta na lista
     s3 = sobrevivente(1)
@@ -50,7 +59,11 @@ def test_questao2():
     s5 = sobrevivente(1)
     list.extend([s3,s4,s5])
     list.remove(s)
-    assert s.quantosDiasSobreviveriamos(list) == 10
+    ok = s.quantosDiasSobreviveriamos(list) == 10
+    if (ok):
+        print("[OK] para 4 sobreviventes, e esse nao pre-incluso na lista")
+    else:
+        print("[FAIL] 4 sobreviventes e esse nao pre-incluso na lista")
 
     #5 sobreviventes com taxas diferentes
     s.comidaPorDia = 2
@@ -58,4 +71,12 @@ def test_questao2():
     s3.comidaPorDia = 3
     s4.comidaPorDia = 3
     list = [s,s2,s3,s4]
-    assert s.quantosDiasSobreviveriamos(list) == 5
+    ok = s.quantosDiasSobreviveriamos(list) == 5
+    if (ok):
+        print("[OK] 5 sobreviventes com taxas diferentes de comida")
+    else:
+        print("[FAIL] 5 sobreviventes com taxas diferentes de comida")
+
+
+test_questao1()
+test_questao2()
